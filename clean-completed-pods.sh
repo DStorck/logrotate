2472bac1bbbada35972a637d30c48e93137f8625
Deirdre Storck <deirdre.storck@gmail.com>
2017-02-03 09:05:54 -0800

    added original clean-pods code with echo debug inserted

--- clean-completed-pods.sh
+++ clean-completed-pods.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-echo "swanky swan is not funny" > swankyswan.log
+app_logs=$ALLAPPLICATIONLOGS
+echo "testing application logs variable creation: "
+echo $app_logs
+for app_dir in $(find "$app_logs" -Bmin +30 -type d -maxdepth 1 -mindepth 1); do
+  total_files=$(find "$app_dir" -type f | wc -l )
+  empty_files=$(find "$app_dir" -size 0 -type f | wc -l)
+  if [[ $total_files -eq $empty_files ]]; then
+    echo "delete dir ${app_dir}"
+    rm -rf "${app_dir}"
+  fi
+done
