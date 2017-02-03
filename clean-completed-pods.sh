4357cf114474d95e927aac8026070adf7d709385
Deirdre Storck <deirdre.storck@gmail.com>
2017-02-03 09:19:18 -0800

    removed bmin for test

--- clean-completed-pods.sh
+++ clean-completed-pods.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 app_logs=$ALLAPPLICATIONLOGS
-echo "testing application logs variable creation: "
-echo $app_logs
-for app_dir in $(find "$app_logs" -Bmin +30 -type d -maxdepth 1 -mindepth 1); do
+
+for app_dir in $(find "$app_logs" -type d -maxdepth 1 -mindepth 1); do
   total_files=$(find "$app_dir" -type f | wc -l )
   empty_files=$(find "$app_dir" -size 0 -type f | wc -l)
   if [[ $total_files -eq $empty_files ]]; then
