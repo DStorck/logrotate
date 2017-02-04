a79d5257792e19e1d190cf609c48e31de0f2043c
Deirdre Storck <deirdre.storck@gmail.com>
2017-02-03 16:10:35 -0800

    experimenting with clean-pods

--- clean-completed-pods.sh
+++ clean-completed-pods.sh
@@ -3,10 +3,6 @@
 app_logs=$ALLAPPLICATIONLOGS
 
 for app_dir in $(find "$app_logs" -type d -maxdepth 1 -mindepth 1); do
-  total_files=$(find "$app_dir" -type f | wc -l )
-  empty_files=$(find "$app_dir" -size 0 -type f | wc -l)
-  if [[ $total_files -eq $empty_files ]]; then
-    echo "delete dir ${app_dir}"
-    rm -rf "${app_dir}"
-  fi
+  echo "${app_dir}" >> found-directory.txt
+
 done
